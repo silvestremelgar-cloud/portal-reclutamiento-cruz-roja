12f05961b494ed3aa8092633723cf32af9fa6aa5
silvestremelgar-cloud <silvestre.melgar@uab.edu.bo>
2026-08-09 22:44:16 -0400

    Validar formulario

--- script.js
+++ script.js
@@ -5,3 +5,27 @@ botonTema.addEventListener("click", function () {
         botonTema.textContent="Modo claro"; }else{
         botonTema.textContent="Modo oscuro";}
 });
+
+const formulario = document.querySelector(".contact-form");
+const nombre = document.querySelector("#nombre");
+const correo = document.querySelector("#correo");
+const mensajeFormulario = document.querySelector("#mensaje-formulario");
+formulario.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const nombreValor = nombre.value.trim();
+    const correoValor = correo.value.trim();
+    mensajeFormulario.classList.remove("error");
+    mensajeFormulario.classList.remove("exito");
+    if(nombreValor === ""){
+        mensajeFormulario.textContent = "Por favor, ingresa tu nombre.";
+        mensajeFormulario.classList.add("error");
+        return;
+    }
+    if(!correoValor.includes("@")){
+        mensajeFormulario.textContent = "El correo debe contener una arroba (@).";
+        mensajeFormulario.classList.add("error");
+        return;
+    }
+    mensajeFormulario.textContent = "¡Mensaje enviado correctamente!";
+    mensajeFormulario.classList.add("exito");
+});
